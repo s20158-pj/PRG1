8ba6c9b306272e979903fe9dbe9ae3f66a7376fc
Patryk Klimek <s20158@pjwstk.edu.pl>
2019-10-20 21:09:15 +0200

    Zadanie programistyczne z cwiczen PRG1, 20.10.2019.

---
+++ zad4.cpp
@@ -0,0 +1,18 @@
+#include <iostream> //kamil surma patryk klimek
+using namespace std;
+int main(){
+
+int i;
+int suma;
+cin >> i;
+do
+{
+cout << suma +=i;
+while(i!=0)
+
+count
+ 
+
+
+return 0;
+}
